13bbb7ac923b29668bbbdc41b9bc358df4030fa1
Ian Costanzo <ian@anon-solutions.ca>
2019-06-12 14:12:35 -0700

    Initial commit of BCCA demo
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

---
+++ openshift/settings.local.sh
@@ -0,0 +1,21 @@
+export PROJECT_NAMESPACE="eao-iuc"
+export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../openshift}
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden
+# with the values of GIT_URI and GIT_REF
+export skip_git_overrides="schema-spy-build.json solr-base-build.json backup-build.json"
+export GIT_URI="https://github.com/esune/eao-inspections-evidence-locker.git"
+export GIT_REF="master"
+
+# The project components
+export components="tob-db tob-solr tob-api tob-web tob-wallet tob-backup eao-agent eao-pipeline"
+
+# The builds to be triggered after buildconfigs created (not auto-triggered)
+export builds=""
+
+# The images to be tagged after build
+export images="angular-on-nginx django eao-agent mara solr schema-spy"
+
+# The routes for the project
+export routes="angular-on-nginx django solr schema-spy"
---
+++ starter-kit/agent/bcca-agent/load-creds.sh
@@ -0,0 +1,3 @@
+python test/issueCred.py testdata/bcreg/*.json --url http://localhost:5000/bcreg
+python test/issueCred.py testdata/bcca/*.json
+
